5f08fb2c525152891837941639d917231e5cd029
Jain <jaibh01@ca.com>
2017-01-30 06:18:52 +0000

    adding reducer

--- reducer.go
+++ reducer.go
@@ -1,1 +1,18 @@
 package gowrapper
+
+type ReducerFunc func(Wrap, Wrap) Wrap
+
+func Reducer(fn ReducerFunc, in chan Wrap) chan Wrap {
+	defer close(in)
+	out := make(chan Wrap)
+	var Wrap accum
+	for ele := range in {
+		if accum == nil {
+			accum = ele
+		} else {
+			accum = fn(accum, ele)
+		}
+	}
+	out <- accum
+	return out
+}
